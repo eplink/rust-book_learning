cce705b9b5252d0315e3bbd47d52203dfa0747fe
tibet <tibet@eplink.ru>
2022-08-15 16:24:12 +0500

    change variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 const MINUT_HOUR:u32 = 60;
 
 fn main() {
@@ -52,6 +53,27 @@ fn main() {
     let tup = (500, 6.4, 1);
     let (x , y, z) = tup;
     println!("x = {x}, y = {y}, z = {z}");
+    let y = tup.1;
+    println!("tup.1 = {y}");
+
+    // array
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 
 
 }
