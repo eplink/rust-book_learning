20f5d766c5e72612eceac552dd43ec026444039a
tibet <tibet@eplink.ru>
2022-08-19 16:28:04 +0500

    Очередной пример

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = print_labeled_measurement(5);
+    println! ("Возвращаемое значение :{x}");
+}
+
+fn print_labeled_measurement(value: i32) -> i32 {
+    // значение функции - параметр плюс 3
+    value + 3
 }
