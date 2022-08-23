dac19eb54ee118508b4fe5dba803e08ef8f96410
tibet <tibet@eplink.ru>
2022-08-23 18:14:26 +0500

    очередное изменение

--- homework_glava3/src/main.rs
+++ homework_glava3/src/main.rs
@@ -1,9 +1,13 @@
 use std::io;
 
-
 fn main() {
-    println!("Введите градусы по Фаренгейту");
+    input_and_convert(); 
+    generate_fib();
+}
 
+// Перевод введенных градусов Фаренгейта в Градусы Цельсия c
+fn input_and_convert() {
+    println!("Введите градусы по Фаренгейту");
     let mut grad_f  = String::new();
     
     io::stdin()
@@ -13,14 +17,46 @@ fn main() {
     let grad_f:i32 = grad_f.trim()
                             .parse::<i32>().expect("Ошибка ввода!");
 
-    println!("Введено градусов по фаренгейту: {grad_f}");
-    
-    let grad_c:i32 = convert_to_celsius(grad_f);
-    
-    println!("В градусах цельсия: {grad_c}");
-    
+    match convert_to_celsius(&grad_f) {
+        Ok(grad) => println!("в градусах цельсия это: {0}",grad),
+        Err(err) => println!("Ошибка: {0}",err),
+    }
 }
 
-fn convert_to_celsius(grad_f :i32) -> i32 {
-    (grad_f-32) * 10 / 18
+// функция конвертации градусов фаренгейта в градусы цельсия
+fn convert_to_celsius(grad_f: &i32) -> Result <i32, String> {
+    const MIN_FH:i32 = -459; // абсолютный ноль по Фаренгейту
+    if grad_f.to_owned() > MIN_FH {
+        return  Ok((grad_f.to_owned() - 32) * 5 / 9);
+    } else {
+        return Err("Переданное значение меньше абсолютного нуля!".to_owned());
+    };
+}
+
+// Генерирование n-го числа Фибоначчи
+fn generate_fib() {
+    println!("Введите номер числа Фибоначчи ");
+    let mut n_fib  = String::new();
+    
+    io::stdin()
+        .read_line(&mut n_fib)
+        .expect("Неправильный ввод!");
+    
+    let n_fib:u32 = n_fib.trim()
+                            .parse::<u32>().expect("Ошибка ввода!");
+
+    match gen_fib (&n_fib) {
+        Ok(fib) => println!("Число: {0}",fib),
+        Err(err) => println!("Ошибка: {0}",err),
+    }
+}
+// функция генерации числа фибоначи в цикле
+fn gen_fib (n_fib: &u32) -> Result <u32, String> {
+    const MAX_N:u32 = 47; // максимальный N для 32 разрядного числа
+    if n_fib.to_owned() > MAX_N {
+        return Err("Сгенерированное значение будет больше возможного".to_owned());        
+    };
+
+    return  Ok(n_fib.to_owned());
+
 }
